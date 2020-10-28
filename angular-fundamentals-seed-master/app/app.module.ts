bdd1ff40f584b00cd9dc1cb729d13fdba3accab6
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-10-27 22:12:32 -0400

    finished property binding lesson

---
+++ angular-fundamentals-seed-master/app/app.module.ts
@@ -0,0 +1,20 @@
+import {NgModule } from '@angular/core'; 
+import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent} from './app.component' ;
+import { CommonModule} from '@angular/common'
+
+
+@NgModule({
+  declarations: [
+     AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule, 
+    CommonModule
+  ],
+
+  bootstrap: [AppComponent]
+})
+
+export class AppModule{ }
