7a3d51c95c328c69f90665b163e9304c20634885
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-11-07 18:34:09 -0500

    set up for the ngfor video

--- angular-fundamentals-seed-master/app/app.component.ts
+++ angular-fundamentals-seed-master/app/app.component.ts
@@ -1,36 +1,53 @@
-import { Component} from '@angular/core'; 
+import { Component } from '@angular/core';
 
-
-@Component({
-  selector:`app-root`, 
-  styleUrls: [ `app.component.scss`],
-  template:`
-  <div class="app"> 
-    
-
-  <input  type = 'text'
-  [value]="name"
-  (input)="handleChange($event.target.value)" >
-  <div *ngIf="name.length > 2">
-  Searching for ...{{name}}</div>
-  </div>
-  `
-  
-})
-export class AppComponent {
-  
-  title: string;
-  
-  name: string = 'Wes';
-  handleChange(value: string) {
- this.name = value; 
-  }
-
-  
-  constructor() {
-   this.title = 'Ultimate Angular'; 
-
-  }
-
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
 }
 
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </template>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+    </div>
+  `
+})
+export class AppComponent {
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
+}
--- angular-fundamentals-seed-master/app/app.module.ts
+++ angular-fundamentals-seed-master/app/app.module.ts
@@ -1,23 +1,17 @@
-import {NgModule } from '@angular/core'; 
-import {BrowserModule} from '@angular/platform-browser';
-import { AppComponent} from './app.component' ;
-import { CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-     AppComponent,
-    
+    AppComponent
   ],
   imports: [
-    BrowserModule, 
-    CommonModule, 
-
+    BrowserModule,
+    CommonModule
   ],
-
   bootstrap: [AppComponent]
 })
-
-export class AppModule{ }
+export class AppModule {}
