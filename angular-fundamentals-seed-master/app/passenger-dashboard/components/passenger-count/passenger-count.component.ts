adeca40b730d5a3d464c8c3bd7ecc21b2d17b89c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-11-14 21:48:23 -0500

    working on ddumb component video

---
+++ angular-fundamentals-seed-master/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core'; 
+
+
+@Component({
+    selector:'passenger-count',
+    templateUrl: './passenger-count.component.html'
+})
+
+export class PassengerCountComponent {
+    constructor() {
+
+    }
+}
---
+++ angular-fundamentals-seed-master/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core'; 
+
+
+@Component({
+    selector:'passenger-detail',
+    templateUrl: './passenger-detail.component.html'
+})
+
+export class PassengerDetailComponent {
+    constructor() {
+
+    }
+}
