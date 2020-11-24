b7180ebdc55475c2abc156d20170e64baf256937
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-11-23 19:16:21 -0500

    finished dynamic input video

--- angular-fundamentals-seed-master/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ angular-fundamentals-seed-master/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component} from '@angular/core'; 
+import { Component, Input} from '@angular/core'; 
+import { Passenger } from '../../models/passenger.interface';
 
 
 @Component({
     selector:'passenger-detail',
+    styleUrls: ['passenger-detail.component.scss'],
     templateUrl: './passenger-detail.component.html'
 })
 
 export class PassengerDetailComponent {
+    @Input()
+    detail: Passenger;
     constructor() {
 
     }
