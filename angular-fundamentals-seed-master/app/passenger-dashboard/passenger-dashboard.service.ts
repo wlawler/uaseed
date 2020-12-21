138b9140541898f849d964f1a2d5b532b6b01db6
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-12-20 20:33:13 -0500

    finished promise video

--- angular-fundamentals-seed-master/app/passenger-dashboard/passenger-dashboard.service.ts
+++ angular-fundamentals-seed-master/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -2,7 +2,7 @@ import { Passenger } from "./models/passenger.interface";
 import { Http, Response, Headers, RequestOptions} from '@angular/http';
 import { Injectable} from '@angular/core';
 import { Observable } from 'rxjs/Observable'; 
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
  
 
 const PASSENGER_API: string = '/api/passengers';
@@ -11,13 +11,14 @@ const PASSENGER_API: string = '/api/passengers';
 export class PassengerDashboardService { 
     constructor(private http: Http) {}  
     
-    getPassengers(): Observable <Passenger[]> {
+    getPassengers(): Promise <Passenger[]> {
        return this.http
        .get(PASSENGER_API) 
-       .map((response: Response) => response.json()); 
+       .toPromise()
+       .then((response: Response) => response.json()); 
     }
 
-    updatePassenger(passenger: Passenger): Observable<Passenger> {
+    updatePassenger(passenger: Passenger): Promise<Passenger> {
       let headers = new Headers ({
         'Content-Type': 'application/json'
       });
@@ -26,13 +27,15 @@ export class PassengerDashboardService {
       });
       return this.http 
       .put(`${PASSENGER_API}/${passenger.id}`, passenger, options )
-      .map((response: Response) => response.json()); 
+      .toPromise()
+      .then((response: Response) => response.json()); 
     }
 
-    removePassenger(passenger: Passenger): Observable<Passenger> {
+    removePassenger(passenger: Passenger): Promise<Passenger> {
       return this.http 
       .delete(`${PASSENGER_API}/${passenger.id}`)
-      .map((response: Response) => response.json()); 
+      .toPromise()
+      .then((response: Response) => response.json()); 
     }
   }
       
