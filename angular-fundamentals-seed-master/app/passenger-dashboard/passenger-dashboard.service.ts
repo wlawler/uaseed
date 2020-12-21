6c884a8c583000ce26358ad47ce4adb7050316e7
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2020-12-20 20:26:37 -0500

    finished options video

--- angular-fundamentals-seed-master/app/passenger-dashboard/passenger-dashboard.service.ts
+++ angular-fundamentals-seed-master/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -2,7 +2,8 @@ import { Passenger } from "./models/passenger.interface";
 import { Http, Response, Headers, RequestOptions} from '@angular/http';
 import { Injectable} from '@angular/core';
 import { Observable } from 'rxjs/Observable'; 
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
+ 
 
 const PASSENGER_API: string = '/api/passengers';
 
@@ -17,8 +18,14 @@ export class PassengerDashboardService {
     }
 
     updatePassenger(passenger: Passenger): Observable<Passenger> {
+      let headers = new Headers ({
+        'Content-Type': 'application/json'
+      });
+      let options = new RequestOptions({
+          headers: headers
+      });
       return this.http 
-      .put(`${PASSENGER_API}/${passenger.id}`, passenger )
+      .put(`${PASSENGER_API}/${passenger.id}`, passenger, options )
       .map((response: Response) => response.json()); 
     }
 
