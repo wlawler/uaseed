b7e2403bdc83949c3502c988445c5cf1a5376f07
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-01-03 18:14:58 -0500

    finished router module video

--- angular-fundamentals-seed-master/app/app.module.ts
+++ angular-fundamentals-seed-master/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule} from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
