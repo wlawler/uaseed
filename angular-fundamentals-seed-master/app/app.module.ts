aa34f113a804de7dd1c1c702b374022ca78ad563
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-01-07 18:29:45 -0500

    finished wildcard route video

--- angular-fundamentals-seed-master/app/app.module.ts
+++ angular-fundamentals-seed-master/app/app.module.ts
@@ -5,16 +5,19 @@ import { RouterModule, Routes} from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent} from './not-found.component'
 import { AppComponent } from './app.component';
 
 const routes: Routes = [ 
-  { path: '', component: HomeComponent, pathMatch: 'full'}
+  { path: '', component: HomeComponent, pathMatch: 'full'}, 
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HomeComponent
+    HomeComponent, 
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-fundamentals-seed-master/app/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'not-found',
+  styleUrls: ['app.component.scss'],
+  template: `
+  <div>
+     Not Found
+   </div>
+  `
+})
+export class NotFoundComponent {
+ 
+}
