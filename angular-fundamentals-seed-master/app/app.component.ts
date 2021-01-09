a3f364b7fd59f65a86ad0f3ca443d223e8a31a3c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-01-08 21:46:44 -0500

    finished ngfor navigation video

--- angular-fundamentals-seed-master/app/app.component.ts
+++ angular-fundamentals-seed-master/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
-
+interface Nav {
+  link: string, 
+  name: string, 
+  exact: boolean 
+}
 
 @Component({
   selector: 'app-root',
@@ -9,21 +13,31 @@ import { Component } from '@angular/core';
   <div class="app">
   <nav class="nav">
   <a 
-  routerLink="/"
+    *ngFor="let item of nav"
+  [routerLink]="item.link"
   routerLinkActive="active"
-  [routerLinkActiveOptions]="{ exact: true}">
-  Home
+  [routerLinkActiveOptions]="{ exact: item.exact}">
+  {{item.name}}
   </a>
-  <a 
-  routerLink="/oops"
-  routerLinkActive="active">
-  404
-  </a>
+
   </nav>
  <router-outlet></router-outlet>
    </div>
   `
 })
 export class AppComponent {
+  nav: Nav[] = [
+    {
+      link: '/', 
+      name: 'home', 
+      exact: true
+    }, 
+    {
+      link: '/oops', 
+      name: 'oops', 
+      exact: false
+    }, 
+
+  ]
  
 }
